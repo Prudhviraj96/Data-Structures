c05fbb4951a9d6fcf54c262fea371ef946b4b095
Prudhvi Raj <prudhviraj63099@gmail.com>
2024-09-11 18:43:08 +0530

    Bubble sort Algorithm

---
+++ Sorting_algorithms/Bubble_sort.py
@@ -0,0 +1,17 @@
+'''Bubble sort is a simple sorting algorithm used in Python to arrange data in a specific order, either ascending or descending.
+It works by repeatedly iterating through a list, comparing and swapping adjacent elements if they are in the wrong order.'''
+
+def bubblesort(arr):
+  qw = 0
+  while qw < len(arr):
+    for i in range(0,len(arr)-1):
+      if arr[i] > arr[i+1]:
+        arr[i] , arr[i+1] = arr[i+1] , arr[i]
+    
+    qw += 1
+  return arr
+
+arr = [5,9,1,2,7,3,8,2]
+print(bubblesort(arr))
+
+    
